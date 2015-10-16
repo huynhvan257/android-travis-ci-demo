c432a6ba789bbc1e7e0fab1ee6ed54a95c374d73
VanHT <vfa.vanht@gmail.com>
2015-10-16 12:50:04 +0900

    + add source

---
+++ app/src/main/java/com/example/van/travisci/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.van.travisci;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by van on 10/16/15.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
