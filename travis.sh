7acd4bd6f5d61fc6ff9bcc187770f6ab727739a2
Huynh Van <huynhvan257@gmail.com>
2015-10-19 11:02:18 +0900

    ! use script build

--- travis.sh
+++ travis.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-if [ ${BUILD_DEBUG} = 1 ]
-    then
-        ./gradlew clean assembleDebug
-        cd app/build/outputs/apk/
-        curl -F "file=@app-debug.apk" -F "token=${TOKEN_DEPLOYGATE}" -F "message=Auto upload" https://deploygate.com/api/users/${DEPLOYGATE}/apps
-    else echo "Don't Build APK"
+if [[ -z "${BUILD_DEBUG}" ]];
+    then echo "... Don't set BUILD_DEBUG"
+    else
+        if [ ${BUILD_DEBUG} = 1 ];
+            then
+                ./gradlew clean assembleDebug
+                cd app/build/outputs/apk/
+                curl -F "file=@app-debug.apk" -F "token=${TOKEN_DEPLOYGATE}" -F "message=Auto upload" https://deploygate.com/api/users/${DEPLOYGATE}/apps
+            else echo "... BUILD_DEBUG != 1. Don't Build Apk"
+        fi
 fi
