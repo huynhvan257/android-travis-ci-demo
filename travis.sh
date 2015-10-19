3c47c82726ae527217223f4e0386b5c28af855aa
Huynh Van <huynhvan257@gmail.com>
2015-10-19 11:08:46 +0900

    no message

--- travis.sh
+++ travis.sh
@@ -7,6 +7,7 @@ if [[ -z "${BUILD_DEBUG}" ]];
                 ./gradlew clean assembleDebug
                 cd app/build/outputs/apk/
                 curl -F "file=@app-debug.apk" -F "token=${TOKEN_DEPLOYGATE}" -F "message=Auto upload" https://deploygate.com/api/users/${DEPLOYGATE}/apps
+                echo "... UPLOAD FINISH"
             else echo "... BUILD_DEBUG != 1. Don't Build Apk"
         fi
 fi
